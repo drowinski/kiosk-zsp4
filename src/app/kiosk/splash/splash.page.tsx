ba13678c7c51be373365610639a3240cba7f6a84
drowinski <dominik.rowinski@protonmail.com>
2024-10-29 03:06:15 +0100

    Create kiosk route, create splash screen with carousel

---
+++ src/app/kiosk/splash/splash.page.tsx
@@ -0,0 +1,50 @@
+import { Card } from '@/components/base/card';
+import { Carousel, CarouselItem } from '@/app/kiosk/splash/_components/carousel';
+import { Link } from '@remix-run/react';
+
+export default function KioskSplashPage() {
+  const coverPhotoUris = new Array(5).fill(undefined).map((_, i) => `/splash/${i}.jpg`);
+
+  return (
+    <Link
+      to="/"
+      className={'flex h-full cursor-default items-center gap-2'}
+    >
+      <div className={'relative flex h-full flex-grow items-center justify-center overflow-hidden'}>
+        <Carousel
+          intervalMs={5000}
+          className={'absolute left-0 top-0 h-full w-full scale-110'}
+        >
+          {coverPhotoUris.map((uri, index) => (
+            <CarouselItem key={index}>
+              <img
+                src={uri}
+                alt={'starodawne zdjęcie'}
+                className={'blur-sm'}
+              />
+            </CarouselItem>
+          ))}
+        </Carousel>
+        <div className={'absolute flex flex-col gap-2'}>
+          <Card className={'flex flex-col items-center bg-primary p-8 text-center text-primary-foreground'}>
+            <span className={'mb-2 text-6xl font-extrabold uppercase'}>
+              Izba
+              <br />
+              Pamięci
+            </span>
+            <span className={'text-3xl font-medium'}>
+              Szkoły Podstawowej nr 4<br />w Augustowie
+            </span>
+          </Card>
+          <Card
+            className={
+              'flex items-center justify-center bg-secondary text-xl font-extrabold uppercase text-secondary-foreground'
+            }
+          >
+            Dotknij aby rozpocząć
+          </Card>
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,7 @@
 import { defineRoutes } from '@remix-run/dev/dist/config/routes';
 
 export const routes = defineRoutes((route) => {
-  route('/', 'home/home.page.tsx', { index: true });
+  route('/', 'kiosk/splash/splash.page.tsx', { index: true });
   route('auth/sign-up', 'auth/sign-up/sign-up.page.tsx');
   route('auth/sign-in', 'auth/sign-in/sign-in.page.tsx');
   route('auth/sign-out', 'auth/sign-out/sign-out.route.ts');
