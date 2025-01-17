02db7d6b15d571e3f9cf51254bde45e719c9e210
drowinski <dominik.rowinski@protonmail.com>
2025-01-17 15:37:19 +0100

    Fix event set to icon instead of button

--- src/app/kiosk/kiosk.layout.tsx
+++ src/app/kiosk/kiosk.layout.tsx
@@ -10,11 +10,15 @@ export default function KioskLayout() {
     <div className={'flex h-full select-none flex-col'}>
       <header className={'p-1'}>
         <Card className={'flex items-center justify-between bg-primary px-2 py-2 text-primary-foreground'}>
-          <Button variant={'ghost'} size={'icon'}>
-            <ArrowLeftIcon onClick={() => navigate('..')} />
+          <Button
+            variant={'ghost'}
+            size={'icon'}
+            onClick={() => navigate('..')}
+          >
+            <ArrowLeftIcon />
           </Button>
           <span className={'text-xl font-bold'}>Izba Pamięci</span>
-          <div className={'basis-0 grow-1'}/>
+          <div className={'grow-1 basis-0'} />
         </Card>
       </header>
       <div className={'h-full overflow-hidden px-1'}>
