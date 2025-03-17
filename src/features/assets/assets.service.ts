04f7b49f7ef5403345ce3cb5754339b1c5675616
drowinski <dominik.rowinski@protonmail.com>
2025-03-17 19:14:47 +0100

    Add try-catch blocks to asset service methods

--- src/features/assets/assets.service.ts
+++ src/features/assets/assets.service.ts
@@ -3,7 +3,7 @@ import { ReadStream } from 'node:fs';
 import { FileManager } from '@/lib/files';
 import { env } from '@/lib/env';
 import * as crypto from 'node:crypto';
-import { Asset, AssetType, NewAsset, UpdatedAsset } from '@/features/assets/assets.validation';
+import { AssetType, NewAsset, UpdatedAsset } from '@/features/assets/assets.validation';
 import * as mime from 'mime-types';
 import ffmpeg from 'fluent-ffmpeg';
 
@@ -21,28 +21,37 @@ export class AssetService {
     this.fileManager = fileManager;
   }
 
-  async uploadAsset(stream: ReadStream, assetData: Omit<NewAsset, 'fileName' | 'assetType'>): Promise<Asset> {
+  async uploadAsset(stream: ReadStream, assetData: Omit<NewAsset, 'fileName' | 'assetType'>): Promise<void> {
     const mimeType = this.normalizeMimeType(assetData.mimeType);
     const assetType = this.getAssetTypeFromMimeType(mimeType);
     const fileName = this.generateFileName(mimeType);
 
-    await this.fileManager.saveFileFromStream(stream, fileName);
-
-    await this.generateThumbnail(fileName, assetType);
-
-    const asset = await this.assetRepository.createAsset({
-      fileName: fileName,
-      mimeType: mimeType,
-      assetType: assetType,
-      description: assetData.description,
-      date: assetData.date
-    });
-
-    if (!asset) {
-      throw new Error('An error occurred while adding asset to repository.');
+    try {
+      await this.fileManager.saveFileFromStream(stream, fileName);
+    } catch (error) {
+      console.error(error);
+      throw new Error('An error occurred while saving asset file.');
     }
 
-    return asset;
+    try {
+      await this.generateThumbnail(fileName, assetType);
+    } catch (error) {
+      console.error(error);
+      throw new Error('An error occurred while generating asset thumbnail.');
+    }
+
+    try {
+      await this.assetRepository.createAsset({
+        fileName: fileName,
+        mimeType: mimeType,
+        assetType: assetType,
+        description: assetData.description,
+        date: assetData.date
+      });
+    } catch (error) {
+      console.error(error);
+      throw new Error('An error occurred while adding asset to repository.');
+    }
   }
 
   async updateAsset(updatedAsset: UpdatedAsset): Promise<void> {
@@ -50,7 +59,13 @@ export class AssetService {
       updatedAsset.mimeType = this.normalizeMimeType(updatedAsset.mimeType);
       updatedAsset.assetType = this.getAssetTypeFromMimeType(updatedAsset.mimeType);
     }
-    await this.assetRepository.updateAsset(updatedAsset);
+
+    try {
+      await this.assetRepository.updateAsset(updatedAsset);
+    } catch (error) {
+      console.error(error);
+      throw new Error('An error occurred while updating asset data.');
+    }
   }
 
   async generateThumbnail(fileName: string, assetType: AssetType): Promise<void> {
