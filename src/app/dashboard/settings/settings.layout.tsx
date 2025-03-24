ba582135a4f793459fff71f3ced627671d1c0880
drowinski <dominik.rowinski@protonmail.com>
2025-03-24 20:30:29 +0100

    Only allow superusers to see User management nav link in settings

--- src/app/dashboard/settings/settings.layout.tsx
+++ src/app/dashboard/settings/settings.layout.tsx
@@ -1,9 +1,25 @@
 import { Card } from '@/components/base/card';
-import { Outlet } from '@remix-run/react';
+import { Outlet, useLoaderData } from '@remix-run/react';
 import { SettingsNav, SettingsNavLink } from '@/app/dashboard/settings/_components/settings-nav';
+import { LoaderFunctionArgs } from '@remix-run/node';
+import { getSession } from '@/features/sessions/sessions.server-utils';
+import { userRepository } from '@/features/users/users.repository';
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  const session = await getSession(request);
+  if (!session.data.userId) {
+    throw new Response(null, { status: 500, statusText: 'Server Error' });
+  }
+  const user = await userRepository.getUserById(session.data.userId);
+  if (!user) {
+    throw new Response(null, { status: 500, statusText: 'Server Error' });
+  }
+  return { isSuperuser: user.isSuperuser };
+}
 
 export default function SettingsLayout() {
+  const { isSuperuser } = useLoaderData<typeof loader>();
+
   return (
     <Card className={'flex overflow-hidden p-0'}>
       <SettingsNav className={'w-1/5 min-w-[20%]'}>
@@ -15,7 +31,7 @@ export default function SettingsLayout() {
           Oś czasu
         </SettingsNavLink>
         <SettingsNavLink to={'splash'}>Slajdy tytułowe</SettingsNavLink>
-        <SettingsNavLink to={'users'}>Użytkownicy</SettingsNavLink>
+        {isSuperuser && <SettingsNavLink to={'users'}>Użytkownicy</SettingsNavLink>}
       </SettingsNav>
       <div className={'grow'}>
         <Outlet />
