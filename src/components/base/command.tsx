84d1059d9cf2e4f89e3e5180120760c702e65739
drowinski <dominik.rowinski@protonmail.com>
2025-03-24 04:30:45 +0100

    Fix className string

--- src/components/base/command.tsx
+++ src/components/base/command.tsx
@@ -117,7 +117,7 @@ export const CommandItem = React.forwardRef<
   <CommandPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex cursor-default select-none items-center gap-2 rounded-xl px-3 py-1.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[selected='true']:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+      'relative flex cursor-default select-none items-center gap-2 rounded-xl px-3 py-1.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[selected="true"]:bg-accent data-[selected="true"]:text-accent-foreground data-[disabled="true"]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
       className
     )}
     {...props}
