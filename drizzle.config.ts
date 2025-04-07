a81d25f50f8f68917900df88ee6197930debdb65
drowinski <dominik.rowinski@protonmail.com>
2025-04-07 23:29:41 +0200

    Update Drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,8 @@ import { env } from '@/lib/.server/env';
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: './src/lib/db/schema.ts',
-  out: './src/lib/db/migrations',
+  schema: './src/lib/.server/db/schema.ts',
+  out: './src/lib/.server/db/migrations',
   dbCredentials: {
     url: env.DB_URL
   },
