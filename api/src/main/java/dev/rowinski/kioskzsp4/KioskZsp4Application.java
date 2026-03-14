050949ad903108724127daf1dd398237ac2fb014
drowinski <dominik.rowinski@protonmail.com>
2026-03-14 19:17:07 +0100

    chore: initialize Spring Boot app

---
+++ api/src/main/java/dev/rowinski/kioskzsp4/KioskZsp4Application.java
@@ -0,0 +1,13 @@
+package dev.rowinski.kioskzsp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskZsp4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KioskZsp4Application.class, args);
+    }
+
+}
